115675850fc05e98643dba205bea32c6163dac38
Maicon Ferreira <m41c0n@outlook.com>
2021-05-21 00:31:01 -0300

    Demo - Delete data

--- src/application/Program.java
+++ src/application/Program.java
@@ -5,6 +5,7 @@ import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 import db.DB;
+import db.DbIntegrityException;
 
 public class Program {
 
@@ -14,16 +15,14 @@ public class Program {
 		try {
 			conn = DB.getConnection();
 			st = conn.prepareStatement(
-					"UPDATE seller "
-					+ "SET BaseSalary = BaseSalary + ? "
+					"DELETE FROM department  "
 					+ "WHERE "
-					+ "(DepartmentId = ?)");
-			st.setDouble(1, 200);
-			st.setInt(2, 2);
+					+ "(Id = ?)");
+			st.setInt(1, 2);
 			int rowsAffected = st.executeUpdate();
 			System.out.println("Done! Rows affected: "+rowsAffected );
 		} catch (SQLException e) {
-			e.printStackTrace();
+		throw new DbIntegrityException(e.getMessage());
 		}
 		finally {
 			DB.closeStatement(st);
